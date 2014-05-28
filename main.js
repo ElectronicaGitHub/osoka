57397973113247564b7c279cf97da7dee8b09270
Philip <molo4nik11@gmail.com>
2014-05-29 01:42:05 +0400

    движения

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 (function() {
 	'use strict'
-	
-	var CANVAS = document.getElementById('canvas'),
+
+	var KEYS = {
+		37: 'LEFT',
+		38: 'UP',
+		39: 'RIGHT',
+		40: 'DOWN',
+		65: 'A',
+		68: 'D',
+		83: 'S',
+		87: 'W',
+		32: 'SPACE'
+	},
+		CANVAS = document.getElementById('canvas'),
 		ctx = canvas.getContext('2d'),
 		CANVASWIDTH = canvas.width,
-		CANVASHEIGHT = canvas.height;
+		CANVASHEIGHT = canvas.height,
+		CHARACTER_SPEED = 3,
+		CHARACTER = null;
 
 	var uuidString = function() {
 	    return Math.random().toString(36).substring(7)
@@ -21,56 +34,135 @@
 
 	var sprite = new Image();
 	sprite.src = "images/Sprites.png";
-	sprite.addEventListener('load', main, false);
-	canvas.addEventListener('keydown', checkKeyDown, false);
-	canvas.addEventListener('keyup', checkKeyUp, false);
+	sprite.addEventListener('load', create, false);
+	document.addEventListener('keydown', checkKeyDown, false);
+	document.addEventListener('keyup', checkKeyUp, false);
 	canvas.addEventListener('click', mouseClicked, false);
 	console.log('sprites loaded')
 
+	function create() {
+		CHARACTER = new Character('Philip', 30, 30, 16, 16);
+		main();
+	}
+
 	function main() {
-		var me = new Character('phil');
-		me.draw(0, 30, 16, 16, 20,20, 2);
-		console.log(me);		
-		// RAF(main);
-	}
-
-	function DrawObject() {
-		this.x = null;
-		this.y = null;
-	}
-	DrawObject.prototype.draw = function(spriteX, spriteY, spriteSizeX, spriteSizeY, x, y, multiple) {
-		ctx.drawImage(sprite, spriteX, spriteY, spriteSizeX, spriteSizeY, x, y, spriteSizeX * multiple, spriteSizeY * multiple);
-	}
-	function Character(name) {
+		ctx.clearRect(0, 0, CANVASWIDTH, CANVASHEIGHT);
+		CHARACTER.draw();
+		RAF(main);
+	}	
+	function Character(name, startX, startY, spriteSizeX, spriteSizeY) {
 		this.id = uuidString();
-		this.name = name || 'character';
+		this.name = name || 'character',
+		this.meta = {
+			drawX : startX,
+			drawY : startY,
+			spriteSizeX : spriteSizeX,
+			spriteSizeY : spriteSizeY,
+			speed : CHARACTER_SPEED,
+			isDownKey : null,
+			isUpKey : null,
+			isRightKey : null,
+			isLeftKey : null,
+			wayGo : 5,
+			animationChangeRate : 200
+		}
 	}
-	Character.prototype = new DrawObject();
+	Character.prototype.draw = function() {
+		this.checkDirection();
 
+		this.shouldUseAlternativeSprite = new Date().getTime() % this.meta.animationChangeRate*2 <= this.meta.animationChangeRate;
+
+		if (this.meta.wayGo == 7 || this.meta.wayGo == 8) {
+	        if (this.shouldUseAlternativeSprite) this.meta.wayGo = 7;   
+	        else this.meta.wayGo = 8;   
+	    }
+		if (this.meta.wayGo == 1 || this.meta.wayGo == 0) { 
+	        if (this.shouldUseAlternativeSprite) this.meta.wayGo = 1;   
+	        else this.meta.wayGo = 0;   
+		}
+		if (this.meta.wayGo == 2 || this.meta.wayGo == 3) { 
+	        if (this.shouldUseAlternativeSprite) this.meta.wayGo = 2;   
+	        else this.meta.wayGo = 3;   
+		}
+		if (this.meta.wayGo == 10 || this.meta.wayGo == 11) { 
+	        if (this.shouldUseAlternativeSprite) this.meta.wayGo = 10;   
+	        else this.meta.wayGo = 11;   
+		}
+		ctx.drawImage(sprite, 0, 30 + this.meta.spriteSizeY * this.meta.wayGo, 
+							 this.meta.spriteSizeX, this.meta.spriteSizeY, 
+							 this.meta.drawX, this.meta.drawY, 
+							 this.meta.spriteSizeX * 2, this.meta.spriteSizeY * 2);
+	}
+	Character.prototype.checkDirection = function() {
+		if (this.meta.isLeftKey && this.meta.isDownKey) {
+			this.meta.drawX += this.meta.speed/Math.sqrt(8);
+			this.meta.drawY -= this.meta.speed/Math.sqrt(8);
+		} 
+		if (this.meta.isLeftKey && this.meta.isUpKey) {
+			this.meta.drawX += this.meta.speed/Math.sqrt(8);
+			this.meta.drawY += this.meta.speed/Math.sqrt(8);
+		} 
+		if (this.meta.isRightKey && this.meta.isDownKey) {
+			this.meta.drawX -= this.meta.speed/Math.sqrt(8);
+			this.meta.drawY -= this.meta.speed/Math.sqrt(8);
+		} 
+		if (this.meta.isRightKey && this.meta.isUpKey) {
+			this.meta.drawX -= this.meta.speed/Math.sqrt(8);
+			this.meta.drawY += this.meta.speed/Math.sqrt(8);
+		} 
+		if (this.meta.isUpKey){
+			this.meta.drawY -= this.meta.speed;
+		}
+		if (this.meta.isRightKey){
+			this.meta.drawX += this.meta.speed;
+		}
+		if (this.meta.isDownKey){
+			this.meta.drawY += this.meta.speed;
+		}
+		if (this.meta.isLeftKey){
+			this.meta.drawX -= this.meta.speed;
+		}
+	}
 
 	function checkKeyDown (e) {
-		var keyID = e.keyCode || e.which;
 		e.preventDefault();
-		if (keyID === 38 || keyID === 87) {     // 38 - up key , 87 - w key 
+		var keyID = e.keyCode || e.which;
+		if (keyID === 38 || keyID === 87) {
+			CHARACTER.meta.isUpKey = true;
+			CHARACTER.meta.wayGo = 10;
 		}
-		if (keyID === 39 || keyID === 68) {     // 39 - right key , 68 - d key
+		if (keyID === 39 || keyID === 68) {
+			CHARACTER.meta.isRightKey = true; 
+			CHARACTER.meta.wayGo = 0;
 		}
-		if (keyID === 40 || keyID === 83) {     // 40 - down key , 83 - s key 
+		if (keyID === 40 || keyID === 83) {
+			CHARACTER.meta.isDownKey = true;
+			CHARACTER.meta.wayGo = 7;
 		}
-		if (keyID === 37 || keyID === 65) {     // 38 - left key , 87 - a key 
+		if (keyID === 37 || keyID === 65) {
+			CHARACTER.meta.isLeftKey = true;
+			CHARACTER.meta.wayGo = 2;
 		}
 	}
 
 	function checkKeyUp (e) {
-		var keyID = e.keyCode || e.which;
 		e.preventDefault();
-		if (keyID === 38 || keyID === 87) {     // 38 - up key , 87 - w key 
+		var keyID = e.keyCode || e.which;
+		if (keyID === 38 || keyID === 87) {
+			CHARACTER.meta.isUpKey = false;
+			CHARACTER.meta.wayGo = 9;
 		}
-		if (keyID === 39 || keyID === 68) {     // 39 - right key , 68 - d key 
+		if (keyID === 39 || keyID === 68) {
+			CHARACTER.meta.isRightKey = false;
+			CHARACTER.meta.wayGo = 5;
 		}
-		if (keyID === 40 || keyID === 83) {     // 40 - down key , 83 - s key
+		if (keyID === 40 || keyID === 83) {
+			CHARACTER.meta.isDownKey = false; 
+			CHARACTER.meta.wayGo = 6;
 		}
-		if (keyID === 37 || keyID === 65) {     // 38 - left key , 87 - a key 
+		if (keyID === 37 || keyID === 65) {
+			CHARACTER.meta.isLeftKey = false;
+			CHARACTER.meta.wayGo = 4;
 		}
 	}
 
@@ -81,5 +173,3 @@
 	}
 
 })()
-
-
