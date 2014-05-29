122005f930199fad786e93b2db2f1a9493e57fd1
Philip <molo4nik11@gmail.com>
2014-05-30 02:11:15 +0400

    отрисовка карты + коллизии персонажей и пуль

--- main.js
+++ main.js
@@ -16,14 +16,31 @@
 		ctx = canvas.getContext('2d'),
 		CANVASWIDTH = canvas.width,
 		CANVASHEIGHT = canvas.height,
-		CHARACTER_SPEED = 3,
+		CHARACTER_SPEED = 5,
 		CHARACTER = null,
 		AMMOARRAY = [],
-		BULLET_SPEED = 10;
+		BULLET_SPEED = 10,
+		STOP_NUMBER = 1;
+
+	// sprites
+	var sprite, background;
 
 	var uuidString = function() {
 	    return Math.random().toString(36).substring(7)
 	}
+
+	var MAP = [
+		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+		[1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+		[1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,1],
+		[1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1],
+		[1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+		[1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+		[1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1],
+		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+	];
 
 	var RAF = window.requestAnimationFrame ||
 		      window.webkitRequestAnimationFrame ||
@@ -34,30 +51,38 @@
 	ctx.webkitImageSmoothingEnabled = false;
 	ctx.mozImageSmoothingEnabled = false;
 
-	var sprite = new Image();
-	sprite.src = "images/Sprites.png";
-	sprite.addEventListener('load', create, false);
-	document.addEventListener('keydown', checkKeyDown, false);
-	document.addEventListener('keyup', checkKeyUp, false);
-	canvas.addEventListener('click', mouseClicked, false);
-	console.log('sprites loaded')
+	(function preload() {
+		sprite = new Image();
+		sprite.src = "images/Sprites.png";
+		background = new Image();
+		background.src = "images/bg.png"
+		sprite.addEventListener('load', create, false);
+		document.addEventListener('keydown', checkKeyDown, false);
+		document.addEventListener('keyup', checkKeyUp, false);
+		canvas.addEventListener('click', mouseClicked, false);
+		console.log('sprites loaded')
+	})();
+
 
 	function create() {
-		CHARACTER = new Character('Philip', 30, 30, 16, 16);
+		CHARACTER = new Character('Philip', 100, 100, 16, 16);
 		main();
 	}
 
 	function main() {
 		ctx.clearRect(0, 0, CANVASWIDTH, CANVASHEIGHT);
+		mapCreate();
+
+
 		CHARACTER.draw();
 
 		for (var i in AMMOARRAY) {
 			var ammo = AMMOARRAY[i];
-			if (ammo.inCanvas()) {
+			if (ammo.inCanvas() && !ammo.collision()) {
 				ammo.draw();
 			} else {
 				AMMOARRAY.splice(i,1);
-				ctx.drawImage(sprite, 4, 222, 6, 6, ammo.FiredStartX-2, ammo.FiredStartY-2, 9, 9);
+				ctx.drawImage(sprite, 4, 222, 6, 6, ammo.meta.drawX-2, ammo.meta.drawY-2, 9, 9);
 			}
 		}
 		RAF(main);
@@ -76,12 +101,23 @@
 			isRightKey : null,
 			isLeftKey : null,
 			wayGo : 5,
-			animationChangeRate : 200
+			animationChangeRate : 200,
+			multiple : 2
+		}
+	}
+	Character.prototype.collision = function() {
+		var x = this.meta.drawX + this.meta.spriteSizeX,
+			y = this.meta.drawY + this.meta.spriteSizeY * 2,
+			blockX = ~~(x / BLOCKS_SIZE.X),
+			blockY = ~~(y / BLOCKS_SIZE.Y);
+		if (MAP[blockY][blockX] == STOP_NUMBER) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	Character.prototype.draw = function() {
 		this.checkDirection();
-
 		this.shouldUseAlternativeSprite = new Date().getTime() % this.meta.animationChangeRate*2 <= this.meta.animationChangeRate;
 
 		if (this.meta.wayGo == 7 || this.meta.wayGo == 8) {
@@ -100,70 +136,92 @@
 	        if (this.shouldUseAlternativeSprite) this.meta.wayGo = 10;   
 	        else this.meta.wayGo = 11;   
 		}
-		ctx.drawImage(sprite, 0, 30 + this.meta.spriteSizeY * this.meta.wayGo, 
-							 this.meta.spriteSizeX, this.meta.spriteSizeY, 
-							 this.meta.drawX, this.meta.drawY, 
-							 this.meta.spriteSizeX * 2, this.meta.spriteSizeY * 2);
+
+		ctx.drawImage(sprite, 0, 
+			30 + this.meta.spriteSizeY * this.meta.wayGo, 
+			this.meta.spriteSizeX, this.meta.spriteSizeY, 
+			this.meta.drawX, this.meta.drawY, 
+			this.meta.spriteSizeX * this.meta.multiple, this.meta.spriteSizeY * this.meta.multiple);
 	}
 	Character.prototype.checkDirection = function() {
-		if (this.meta.isLeftKey && this.meta.isDownKey) {
-			this.meta.drawX += this.meta.speed/Math.sqrt(8);
-			this.meta.drawY -= this.meta.speed/Math.sqrt(8);
-		} 
-		if (this.meta.isLeftKey && this.meta.isUpKey) {
-			this.meta.drawX += this.meta.speed/Math.sqrt(8);
-			this.meta.drawY += this.meta.speed/Math.sqrt(8);
-		} 
-		if (this.meta.isRightKey && this.meta.isDownKey) {
-			this.meta.drawX -= this.meta.speed/Math.sqrt(8);
-			this.meta.drawY -= this.meta.speed/Math.sqrt(8);
-		} 
-		if (this.meta.isRightKey && this.meta.isUpKey) {
-			this.meta.drawX -= this.meta.speed/Math.sqrt(8);
-			this.meta.drawY += this.meta.speed/Math.sqrt(8);
-		} 
-		if (this.meta.isUpKey){
-			this.meta.drawY -= this.meta.speed;
-		}
-		if (this.meta.isRightKey){
-			this.meta.drawX += this.meta.speed;
-		}
-		if (this.meta.isDownKey){
-			this.meta.drawY += this.meta.speed;
-		}
-		if (this.meta.isLeftKey){
-			this.meta.drawX -= this.meta.speed;
-		}
+		this.meta.lastX = this.meta.drawX;
+		this.meta.lastY = this.meta.drawY;
+
+			if (this.meta.isLeftKey && this.meta.isDownKey) {
+				this.meta.drawX += this.meta.speed/Math.sqrt(8);
+				this.meta.drawY -= this.meta.speed/Math.sqrt(8);
+			} 
+			if (this.meta.isLeftKey && this.meta.isUpKey) {
+				this.meta.drawX += this.meta.speed/Math.sqrt(8);
+				this.meta.drawY += this.meta.speed/Math.sqrt(8);
+			} 
+			if (this.meta.isRightKey && this.meta.isDownKey) {
+				this.meta.drawX -= this.meta.speed/Math.sqrt(8);
+				this.meta.drawY -= this.meta.speed/Math.sqrt(8);
+			} 
+			if (this.meta.isRightKey && this.meta.isUpKey) {
+				this.meta.drawX -= this.meta.speed/Math.sqrt(8);
+				this.meta.drawY += this.meta.speed/Math.sqrt(8);
+			} 
+			if (this.meta.isUpKey){
+				this.meta.drawY -= this.meta.speed;
+			}
+			if (this.meta.isRightKey){
+				this.meta.drawX += this.meta.speed;
+			}
+			if (this.meta.isDownKey){
+				this.meta.drawY += this.meta.speed;
+			}
+			if (this.meta.isLeftKey){
+				this.meta.drawX -= this.meta.speed;
+			}
+			if (this.collision()) {
+				this.meta.drawX = this.meta.lastX;
+				this.meta.drawY = this.meta.lastY;
+			}	
 	}
 
 	function Ammo(xS,yS,xE,yE) {
-		this.FiredStartX = xS;
-		this.FiredStartY = yS;
+	    this.meta = {
+			drawX : xS,
+			drawY : yS
+		}
 		this.FiredEndX = xE;
 		this.FiredEndY = yE;
 		this.speed = BULLET_SPEED;
-	    this.angleRadian = Math.atan2(this.FiredEndY - this.FiredStartY, this.FiredEndX - this.FiredStartX);
+	    this.angleRadian = Math.atan2(this.FiredEndY - this.meta.drawY, this.FiredEndX - this.meta.drawX);
 	    this.rotation = (this.angleRadian * 180 / Math.PI);
 	    this.ammoID = uuidString();
 	    this.initiator = CHARACTER.ID;
 	}
+	Ammo.prototype.collision = function() {
+		var x = this.meta.drawX,
+			y = this.meta.drawY,
+			blockX = ~~(x / BLOCKS_SIZE.X),
+			blockY = ~~(y / BLOCKS_SIZE.Y);
+		if (MAP[blockY][blockX] == STOP_NUMBER) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	Ammo.prototype.draw = function() {
-	    this.FiredStartX +=  Math.cos(this.angleRadian) * this.speed;
-	    this.FiredStartY +=  Math.sin(this.angleRadian) * this.speed;
+	    this.meta.drawX +=  Math.cos(this.angleRadian) * this.speed;
+	    this.meta.drawY +=  Math.sin(this.angleRadian) * this.speed;
 	  
 	 	ctx.save();
-		ctx.translate(this.FiredStartX, this.FiredStartY);
+		ctx.translate(this.meta.drawX, this.meta.drawY);
 		ctx.rotate(this.angleRadian + Math.PI/2);
 		ctx.drawImage(sprite , 0 , 222 , 4 , 12 , 0 , 0 , 4 , 16 );
 		ctx.restore();
 	}
 
 	Ammo.prototype.inCanvas = function() {
-		if (this.FiredStartX < CANVASWIDTH && 
-	        this.FiredStartX > 0 && 
-	        this.FiredStartY < CANVASHEIGHT && 
-   	        this.FiredStartY > 0) {
+		if (this.meta.drawX < CANVASWIDTH && 
+	        this.meta.drawX > 0 && 
+	        this.meta.drawY < CANVASHEIGHT && 
+   	        this.meta.drawY > 0) {
 			return true;
 		} 
 		else return false;
@@ -217,8 +275,26 @@
 		AMMOARRAY.push(new Ammo(CHARACTER.meta.drawX + CHARACTER.meta.spriteSizeX, 
 								CHARACTER.meta.drawY + CHARACTER.meta.spriteSizeY, 
 								posX, posY));
-
-
 	}
+
+	var BLOCKS_FOR_X = MAP[0].length;
+	var BLOCKS_FOR_Y = MAP.length;
+	var BLOCKS_SIZE = {
+		X : CANVASWIDTH / BLOCKS_FOR_X,
+		Y : CANVASHEIGHT / BLOCKS_FOR_Y
+	};
+	console.log(BLOCKS_FOR_X, BLOCKS_FOR_Y, BLOCKS_SIZE)
+
+	function mapCreate() {
+		for (var i = MAP.length-1; i >= 0; i--) {
+			for (var j = MAP[i].length-1; j >= 0; j--) {
+				ctx.drawImage(background, 0 + 50 * MAP[i][j], 0, 
+										  50, 50, 
+										  BLOCKS_SIZE.X * j, BLOCKS_SIZE.Y * i, 
+										  BLOCKS_SIZE.X, BLOCKS_SIZE.Y)
+			}
+		}
+	}
+	
 
 })()
