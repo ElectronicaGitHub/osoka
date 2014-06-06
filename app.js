25de07f86b47de5eb6793d0bfb8c3ed7c96e2336
Philip <molo4nik11@gmail.com>
2014-06-06 09:23:56 +0400

    переделан сокет на сокетио и начат старт делаться

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
+var bodyParser = require('body-parser');
 // var faye = require('faye');
 var path = require('path');
 // var io = require('socket.io')(http);
 
 var app = express();
+app.use(bodyParser());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 var server = http.createServer(app);
 // bayeux = new faye.NodeAdapter({mount: '/game', timeout: 45});
 // bayeux.attach(server);
@@ -19,10 +23,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.render('index');
 });
+app.post('/game', function (req, res, err) {
+    GAMES.push(req.body);
+    console.log(GAMES);
+    res.send({
+        data: req.body,
+        message: 'Room created'
+    })
+})
 
 server.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+var GAMES = [];
 
 var players = {};
 var io = require('socket.io').listen(server);
@@ -45,11 +59,21 @@ io.on('connection', function(socket) {
     });
     socket.on('movement', function(data) {
         socket.broadcast.emit('movement', data);
-    })
+    });
     socket.on('ammos', function(data) {
         socket.broadcast.emit('ammos', data);
-    })
+    });
     socket.on('ammo exit', function(data) {
         socket.broadcast.emit('ammo exit', data);
+    });
+
+    // meta channel
+    socket.on('main', function(data) {
+        console.log('//// main ////');
+        var id = 'test_' + data.channel;
+        socket.on(id, function(data) {
+            console.log(data);
+        })
+
     })
 })
